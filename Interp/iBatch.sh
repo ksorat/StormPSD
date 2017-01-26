d44df06fc7fefae9a3adfb1a82107721373d150c
ksorat <kareem.sorathia@gmail.com>
2017-01-26 10:00:15 -0500

    Fast push

--- Interp/iBatch.sh
+++ Interp/iBatch.sh
@@ -4,7 +4,8 @@
 #Format, lfmtpBatch.sh <STUB> B0 Bend
 #Format, iBatch.sh D0 DEnd
 
-export NAME="StormPSD"
+export NSTUB="StormI"
+
 export TAG="LR60-Quad-15s-AEH_mhd"
 export DATESTUB="2013-03"
 export MAP="$HOME/Work/maps/Quad2Hirez.h5"
@@ -22,8 +23,8 @@ do
 
 	printf -v DD %"02d" $d
 	export FSTUB=${TAG}_$DATESTUB-${DD}T
-	echo $FSTUB
 	export LOG="Interp.$DD.%I.log" #Set names for log files
-
-	bsub -a poe -P "UJHB0003" -W $WALL -n 1 -q $QUEUE -J "StormPSD[1-24]%5" -e ${LOG} -o ${LOG} "iBlock.sh $FSTUB $MAP $LFMDIR"
+	export NAME="${NSTUB}${DD}"
+	echo "Submitting $NAME to work on $FSTUB"
+	bsub -a poe -P "UJHB0003" -W $WALL -n 1 -q $QUEUE -J "${NAME}[1-24]" -e ${LOG} -o ${LOG} "iBlock.sh $FSTUB $MAP $LFMDIR"
 done
