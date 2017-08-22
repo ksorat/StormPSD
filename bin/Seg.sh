e59de4f352aca0a658dd56563b87cd5afad4d7e0
ksorat <kareem.sorathia@gmail.com>
2017-08-22 08:53:46 -0400

    Lazy push message

--- bin/Seg.sh
+++ bin/Seg.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
 #Moves unfinished blocks out of the way
-OUTDIR="unfin"
 INJSIZE="900M"
 TRAPSIZE="100M"
 
-COMS="ls -lh"
+#COMS="ls -lh"
+COMS="mv -t unfin"
 find Trap -name "*.h5part" -size -${TRAPSIZE} -exec ${COMS} {} +
 find Inj* -name "*.h5part" -size -${INJSIZE}  -exec ${COMS} {} +
