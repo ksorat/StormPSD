4018d19603fbda70a0349518a27f06b28d918af9
ksorat <kareem.sorathia@gmail.com>
2017-08-31 13:04:52 -0400

    Lazy push message

---
+++ StormBatch.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#Uses arguments to submit batch of jobs
+#StormBatch.sh B0 Bend
+
+#Will submit jobs w/ runIDs in [B0,Bend]
+#B0/Bend default to 1/10, "StormInj"
+
+export B0=${1:-1}
+export BEND=${2:-10}
+export STUB=$3
+
+
+export WALL="12:00" #Wallclock time
+export NUMT="16" #Run with NUMT threads on 1 node
+
+export QUEUE="regular"
+export LOG="$STUB.%I.log" #Set names for log files
+
+bsub -a poe -P "UJHB0003" -W $WALL -n 1 -q $QUEUE -J "$STUB[$B0-$BEND]" -e ${LOG} -o ${LOG} "StormBlock.sh $STUB"
