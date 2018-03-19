be471a409771ece94dcb89e81818023e836a9987
ksorat <kareem.sorathia@gmail.com>
2018-03-19 13:50:57 -0400

    Lazy push message

---
+++ genPics/wind/chkWind.py
@@ -0,0 +1,19 @@
+import numpy as np
+import glob
+import lfmPreproc as lfmpre
+from pyhdf.SD import SD, SDC
+
+lfmDir = "os.path.expanduser('~') + "/Work/lfmHDFs"
+fIns = glob.glob(lfmDir+"/*17T*.hdf")
+
+Nf = len(fIns)
+
+t = np.zeros(Nf)
+N = np.zeros(Nf)
+
+for i in range(Nf):
+	fIn = fIns[i]
+	hdffile = SD(fIn)
+	t[i] = hdffile.attributes().get('time')
+	D3 = lfm.getHDFScl(hdffile,"rho")
+	print("fIn = %s / T = %f"%(fIn,t[i]))
